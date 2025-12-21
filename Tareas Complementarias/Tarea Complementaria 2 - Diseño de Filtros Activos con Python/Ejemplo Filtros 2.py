eafd6e9fb5fa872ae5af5dadd51cb909d9eb7b87
Ernesto Monja <ernesto_09_@live.com.ar>
2025-12-21 17:20:21 -0300

    Update Ejemplo Filtros 2.py

--- Tareas Complementarias/Tarea Complementaria 2 - Diseño de Filtros Activos con Python/Ejemplo Filtros 2.py
+++ Tareas Complementarias/Tarea Complementaria 2 - Diseño de Filtros Activos con Python/Ejemplo Filtros 2.py
@@ -1,19 +1,32 @@
 import numpy as np
 from scipy import signal
-import matplotlib.pyplot as plt
 
-f_c = 1000
-w_c = 2*np.pi*f_c
-z, p, k = signal.butter(2, w_c, btype='lowpass', analog=True, output = 'zpk')
+
+## Ancho de Banda a Rechazar:
+f1 = 1000                                       # Frecuencia Inferior: 1000 [Hz]
+f2 = 2000                                       # Frecuencia Superior: 2000 [Hz]
+Wbw = [2*np.pi*f1, 2*np.pi*f2]
+
+## Aproximación de un filtro rechaza banda, analógico y de segundo orden, con un 
+## ripple en la banda de rechazo de 1 [dB] que sigue las frecuencias listadas 
+## anteriormente.
+num, den = signal.cheby1(N = 2,                 # Filtro de 2do Orden
+                        rp = 1,                 # Ripple de 1 [dB]
+                        Wn = Wbw,               # Frecuencias a Rechazar
+                        btype = 'bandstop',     # Filtro rechaza banda
+                        analog = True,          # Filtro analógico
+                        output = 'ba')          # Salida del tipo: F(s) = num/den
+
+## Impresión de los parámetros obtenidos:
+print("Numerador:", num)
+print("Denominador:", den)
+
+## Descomposición en ceros, polos y constante
+z, p, k = signal.tf2zpk(num, den)
 print("Ceros: ", z)
 print("Polos: ", p)
 print("Constante: ", k)
 
-w = np.logspace(1, 5, 1000)
-w, h = signal.freqs(num, den, w)
-
-plt.semilogx(w/(2*np.pi), 20*np.log10(abs(h)))
-plt.xlabel("Frecuencia [Hz]")
-plt.ylabel("Magnitud [dB]")
-plt.grid()
-plt.show()
+## Descomposición en bicuadraticas mediante sos
+sos = signal.tf2sos(num,den)
+print("Coeficientes del SOS:", sos)
